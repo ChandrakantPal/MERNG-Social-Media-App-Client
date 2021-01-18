dc3062a63e5dac64c9c17b2746d20d33bff237f5
ChandrakantPal <sanjay161097@gmail.com>
2021-01-19 01:40:35 +0530

    proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://pacific-waters-17319.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
