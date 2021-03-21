fe300892300f8ae00af61207b61c493f4f1a9d29
ChandrakantPal <sanjay161097@gmail.com>
2021-03-22 03:29:03 +0530

    backend url changed

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,33 +1,33 @@
-import React from "react";
-import App from "./App";
+import React from 'react'
+import App from './App'
 import {
   ApolloClient,
   InMemoryCache,
   createHttpLink,
   ApolloProvider,
-} from "@apollo/client";
-import { setContext } from "apollo-link-context";
+} from '@apollo/client'
+import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-  uri: "https://pacific-waters-17319.herokuapp.com/",
-});
+  uri: 'https://boiling-cliffs-63724.herokuapp.com/',
+})
 
 const authLink = setContext(() => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token')
   return {
     headers: {
-      Authorization: token ? `Bearer ${token}` : "",
+      Authorization: token ? `Bearer ${token}` : '',
     },
-  };
-});
+  }
+})
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-});
+})
 
 export default (
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-);
+)
